5f2a0b4ef7e30ff2f11f939b96070cc0af75f019
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2025-04-19 10:39:35 +0530

    Add keyboard shortcut and improve navbar styles
    Introduce a keyboard shortcut ('r') to redirect to a PDF using `ActionService`. Enhance navbar link hover and focus styles with smoother transitions. Additionally, add an ID to the footer section for better semantic structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, inject} from '@angular/core';
 import {HeroComponent} from './features/hero/hero.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {ProjectsComponent} from './features/projects/projects.component';
@@ -7,6 +7,7 @@ import {PhilosophyComponent} from './features/philosophy/philosophy.component';
 import {CtaComponent} from './features/cta/cta.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {TechStackComponent} from './features/tech-stack/tech-stack.component';
+import {ActionService} from './core/services/action.service';
 
 @Component({
   selector: 'app-root',
@@ -16,5 +17,16 @@ import {TechStackComponent} from './features/tech-stack/tech-stack.component';
 })
 export class AppComponent {
   title = 'portfolio';
+  private action: ActionService = inject(ActionService);
+
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyPress(event: KeyboardEvent) {
+    if (event.key.toLowerCase() === 'r' && !(event.target instanceof HTMLInputElement)) {
+      this.action.redirectTo('AbhisekMohanty.pdf');
+    }
+
+  }
+
 
 }
